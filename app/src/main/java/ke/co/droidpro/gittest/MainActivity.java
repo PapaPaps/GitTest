163f8e058790db1bad1f5dba69d2f04c49cdfb7e
unknown <lgjumatech@gmail.com>
2018-04-18 05:52:45 +0300

    Initial commit

---
+++ app/src/main/java/ke/co/droidpro/gittest/MainActivity.java
@@ -0,0 +1,17 @@
+package ke.co.droidpro.gittest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void nextClick(View view) {
+    }
+}
